fb08465678c4515de9eeaa3d8cbab9f9e3faec7f
Akshat Jain <jainAkshat_02@protonmail.com>
2025-05-18 12:55:18 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use(express.json()); //allows us to accept JSON data
 app.use(express.urlencoded({ extended: true })); //allows us to accept URL encoded data
 app.use(cookieParser());
 app.use(cors(
-    {origin: 'https://localhost:5173', 
+    {origin: 'https://pictorial-kappa.vercel.app/', 
      credentials: true}
 )); 
 
