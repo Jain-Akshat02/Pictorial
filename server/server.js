d5a426bc1a348ea9a1217c9e80528274c8207436
AkshatJain <jainakshat_02@protonmail.com>
2025-05-24 13:02:19 +0530

    some changes in server.js

--- server/server.js
+++ server/server.js
@@ -23,7 +23,12 @@ app.use(express.urlencoded({ extended: true })); //allows us to accept URL encod
 app.use(cookieParser());
 
 // CORS configuration
-app.use(cors()); 
+app.use(cors(
+    {
+        origin: 'https://pictorial-ebon.vercel.app/',
+        credentials: true
+    }
+)); 
 
 // Security headers
 app.use((req, res, next) => {
