fcac0b81c81f174ea42181c46d7e5b0929ed92c1
AkshatJain <jainakshat_02@protonmail.com>
2025-05-26 14:18:02 +0530

    checking why DB is not connecting in prod

--- server/config/db.js
+++ server/config/db.js
@@ -3,9 +3,18 @@ export const connectDB = async() =>{
     try {
         await mongoose.connect(process.env.MONGO_URI);
         console.log(`Database connected: ${mongoose.connection.host}`);
+
+        // Add connection event listeners
+        mongoose.connection.on('error', err => {
+            console.error('MongoDB connection error:', err);
+        });
+        mongoose.connection.on('disconnected', () => {
+            console.log('MongoDB disconnected');
+        });
         //hello world
     } catch (error) {
         console.log(error);
+        process.exit(1);
     }
 }
 
